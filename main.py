b9c5064f0ce8132158c0a805f3cca7df5c6e78bd
Daneos08 <113821056+Daneos08@users.noreply.github.com>
2023-02-19 09:34:08 +0000

    add class tree, constructor.

---
+++ main.py
@@ -0,0 +1,20 @@
+class Tree:
+  leafs = 53458224
+  age = 67
+  height = 365  
+
+  def __init__(self, type):
+     self.type = type
+    
+  def get_older(self):
+     self.age +- 1
+     
+
+my_tree =Tree()
+
+print(my_tree.age)
+
+print(my_tree.height)
+
+my_tree.get_older()
+
